017e4b06bf63c69f510add5a83a769b33bcf4b01
abdulrahman <abdulrahman@abadc.com.sa>
2025-10-30 20:41:38 +0300

    account_payment_installment

---
+++ account_payment_installment/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    "name": "Account Payment Installment",
+    "summary": """
+        Account Payment Installment""",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/account-addons",
+    "depends": ["account"],
+    "data": ["views/account_payment_register_views.xml"],
+}
